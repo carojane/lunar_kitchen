f3b4547f87a1691d82b40db10373dd3e9d93161e
carojane <carojane@gmail.com>
2014-09-05 14:40:33 -0400

    better output of recipe info when searching by id

--- models/recipe.rb
+++ models/recipe.rb
@@ -24,23 +24,45 @@ class Recipe
     query = 'SELECT recipes.id,
                   recipes.name,
                   recipes.description,
-                  recipes.instructions
+                  recipes.instructions,
+                  ingredients.name AS ingredient
              FROM recipes
+             JOIN ingredients ON recipes.id = ingredients.recipe_id
              WHERE recipes.id = $1'
 
     DatabaseQuery.db_connection do |conn|
       recipe = conn.exec(query, [id])
-      Recipe.new(recipe[0])
+      recipe = recipe.to_a
+      recipe_info = {'id'=>recipe[0]['id'],
+                     'name'=>recipe[0]['name'],
+                     'description'=>recipe[0]['description'],
+                     'instructions'=>recipe[0]['instructions']}
+      ingredients = []
+      recipe.each {|ingredient| ingredients << ingredient['ingredient']}
+      recipe_info['ingredients'] = ingredients
+      Recipe.new(recipe_info)
     end
   end
 
   def description
     @description = @recipe['description']
+    if @description == nil
+      "This recipe doesn't have a description."
+    else
+      @description
+    end
   end
+
   def instructions
     @instructions = @recipe['instructions']
+    if @instructions == nil
+      "This recipe doesn't have any instructions."
+    else
+      @instructions
+    end
   end
+
   def ingredients
-    []
+    recipe[ingredients]
   end
 end
