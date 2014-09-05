56aa2ddba2bab5911c7d7e5ba6a7f333602eb0ea
carojane <carojane@gmail.com>
2014-09-05 14:06:32 -0400

    pulled individul info from db for show.erb

--- models/recipe.rb
+++ models/recipe.rb
@@ -5,6 +5,7 @@ class Recipe
   def initialize(recipe)
     @id = recipe['id']
     @name = recipe['name']
+    @recipe = recipe
   end
 
   def self.all
@@ -17,5 +18,29 @@ class Recipe
       end
     end
     all_recipes
+  end
+
+  def self.find(id)
+    query = 'SELECT recipes.id,
+                  recipes.name,
+                  recipes.description,
+                  recipes.instructions
+             FROM recipes
+             WHERE recipes.id = $1'
+
+    DatabaseQuery.db_connection do |conn|
+      recipe = conn.exec(query, [id])
+      Recipe.new(recipe[0])
+    end
+  end
+
+  def description
+    @description = @recipe['description']
+  end
+  def instructions
+    @instructions = @recipe['instructions']
+  end
+  def ingredients
+    []
   end
 end
